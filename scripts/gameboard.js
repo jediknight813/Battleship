502e06bead52c584c1e50a9be53d564dc4d364ff
-THE-LAST-SAMURAI- <connor813@outlook.com>
2021-09-08 10:24:55 -0500

    added player board and computer board creation

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -1,26 +1,62 @@
+player_board_id = document.getElementById('#computer-grid-container')
+computer_board_id = document.getElementById('#computer-grid-container')
+
 
 class gameboard {
-    constructor(self, name, board, hits, hidden_board, ship_value, MissesAndHits=[]) {
+    constructor(name, div_board=[], board=[], hits=0, hidden_board=[], ship_value=10, MissesAndHits=[], div_board_unlocked=false) {
         this.name = name
+        this.div_board = div_board
         this.board = board
         this.hits = hits
         this.hidden_board = hidden_board
         this.MissesAndHits = MissesAndHits
         this.ship_value = ship_value
+        this.div_board_unlocked = div_board_unlocked
 
     }
 }
 
-player_board_id = document.getElementById('player_board')
+class game {
+    constructor(gameover, current_turn, computer_gameboard, player_gameboard) {
+        this.gameover = gameover
+        this.current_turn = current_turn
+        this.computer_gameboard = computer_gameboard
+        this.player_gameboard = player_gameboard
+    }
 
-computer_board_id = document.getElementById('computer_board')
-
-
-function create_board(board_id) {
-    
-
-    return board
 }
+
+
+function game_manager() {
+    game_variables = new game()
+    game_variables.computer_board = new gameboard("computer")
+    game_variables.player_board = new gameboard("player")
+    create_board(game_variables.player_board, "#player-grid-container")
+    create_board(game_variables.computer_board, "#computer-grid-container")
+
+    console.log(game_variables.player_board.div_board)
+
+}
+
+
+
+function create_board(gameboard, board_id) {
+    let arrays = 10
+    while (arrays > 0) {
+        let array_count = 10
+        let b = []
+        while (array_count > 0) {
+            var div = document.createElement("button");
+            div.className = "cell"
+            div.addEventListener("click", grid_tile_hit);
+            document.getElementById(board_id).appendChild(div)
+            array_count -= 1 
+            b.push(div)
+            }
+        arrays -= 1
+        gameboard.div_board.push(b)
+        }
+    }
 
 
 function create_new_grid(grid_id) {
@@ -32,49 +68,18 @@ function create_new_grid(grid_id) {
     }
 
 
-function createGrid(grid_id) {
-    var div = document.createElement("button");
-    div.className = "cell"
-    div.addEventListener("click", grid_tile_hit);
-    document.getElementById(grid_id).appendChild(div)
-}
-
-
 function grid_tile_hit() {
     this.className = "tile_hit"
+    get_tile_index.call(this)
 }
 
-
-function create_boards() {
-    create_new_grid("#computer-grid-container")
-    create_new_grid("#player-grid-container")
+function get_tile_index(tile) {
+    let x = player_board.indexOf(this)
+    let index_y = 0
+    while (x == -1) {
+        x = player_board[index_y].indexOf(this)
+        index_y += 1
+    }
+    console.log(index_y, + " " + x )
 }
 
-
-// def game(): #starts and manages the game
-//     turn = 0
-//     gameover = False
-//     current_turn = "player"
-//     player_board = gameboard("player", create_board(0, 10), 0, None, 4)
-//     computer_board = gameboard("computer", create_board(0, 10), 0, create_board("?", 10), 5)
-//     computer_board.board = place_ships_randomly_on_board(computer_board.board, [3,2])
-
-//     print_board(player_board.board)
-//     player_board.board = pick_ship_location(player_board.board, [4])
-
-//     while gameover == False:
-//         gameover = check_for_winner(player_board) #handles the players turn
-//         if current_turn == "player" and gameover == False:
-//             print("current turn: " + str(turn))
-//             print(player_board.name + " turn")
-//             attack(computer_board)
-//             current_turn = "computer"
-//             turn += 1
-
-
-//         gameover = check_for_winner(computer_board) #handles the computers turn
-//         if current_turn == "computer" and gameover == False:
-//             print("current turn: " + str(turn))
-//             computer_attack(player_board)
-//             current_turn = "player"
-//             turn += 1
