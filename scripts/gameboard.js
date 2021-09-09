97b433514bd9486d2623e4500ae951320f3bcce4
-THE-LAST-SAMURAI- <connor813@outlook.com>
2021-09-09 14:53:20 -0500

    added a turn check

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -1,5 +1,3 @@
-player_board_id = document.getElementById('#computer-grid-container')
-computer_board_id = document.getElementById('#computer-grid-container')
 const range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
 
 
@@ -31,7 +29,7 @@ class game {
 }
 
 
-function game_manager() {
+function game_setup() {
     game_variables = new game()
     game_variables.computer_gameboard = new gameboard("computer")
     game_variables.player_gameboard = new gameboard("player")
@@ -42,7 +40,26 @@ function game_manager() {
     game_variables.computer_gameboard.hidden_board = create_gameboard()
     game_variables.computer_gameboard.board = create_gameboard()
     game_variables.player_gameboard.ship_list = [3, 4, 2, 2, 1]
+    game_variables.player_gameboard.ships_placed = false
 
+}
+
+
+function game_manager() {
+    if (game_variables.gameover == true) {
+        return "gameover"
+    }
+
+    if (game_variables.current_turn == "player") {
+        document.getElementById("status_text").innerText = "player turn"
+    }
+
+    if (game_variables.current_turn == "computer") {
+        document.getElementById("status_text").innerText = "computer turn"
+    }
+
+
+    setTimeout(game_manager, 1)
 }
 
 
@@ -71,13 +88,7 @@ function create_board(gameboard, board_id, board_name) {
 
 
 function place_player_ships() {
-    //this.className = "hover"
     get_tile_index.call(this)
-}
-
-
-function update_board(params) {
-    
 }
 
 
@@ -104,36 +115,56 @@ function create_new_grid(grid_id) {
     }
 
 
-
-
-
 function place_ship(x, y, board, ship) { 
     length = ship
-
-
     let b = board[y-1][x]
     b.className = "ship_tile"
+    game_variables.player_gameboard.board[y-1][x] = 2
     if (length > 1){
        let a = board[y-1][x+1]
+       game_variables.player_gameboard.board[y-1][x+1] = 2
        a.className = "ship_tile"
     }
         if (length > 2){
            let b = board[y-1][x+2]
+           game_variables.player_gameboard.board[y-1][x+2] = 2
            b.className = "ship_tile"
         }
             if (length > 3){
                let c = board[y-1][x+3]
+               game_variables.player_gameboard.board[y-1][x+3] = 2
                c.className = "ship_tile"
             }
+
+                if (length > 4){
+                    let c = board[y-1][x+4]
+                    game_variables.player_gameboard.board[y-1][x+4] = 2
+                    c.className = "ship_tile"
+                }
+
+
 
     }
 
 
 function grid_tile_hit() {
-    if (game_variables.current_turn == "player" && game_variables.ships_placed == true) {
-        this.className = "tile_hit"
-        get_tile_index.call(this)
-        game_variables.current_turn = "computer"
+    if (game_variables.current_turn == "player" && game_variables.player_gameboard.ships_placed == true) {
+        //game_variables.current_turn = "computer"
+        //this.className = "tile_hit"
+        x = get_computer_tile_index.call(this)
+        console.log(x)
+        if (game_variables.computer_gameboard.board[y][x] != "X") {
+            if (game_variables.computer_gameboard.board[y][x] == 2){
+                this.className = "tile_hit"
+            }
+            if (game_variables.computer_gameboard.board[y][x] == 0) {
+                this.className = "tile_miss"
+            }
+        }
+
+
+
+
     }
 }
 
@@ -145,27 +176,39 @@ function get_tile_index() {
         x = game_variables.player_gameboard.div_board[index_y].indexOf(this)
         index_y += 1
     }
-    pick_ship_location(game_variables.player_gameboard.div_board, x, index_y-1) 
+    if (game_variables.player_gameboard.ships_placed == false) {
+        pick_ship_location(game_variables.player_gameboard.div_board, x, index_y-1) 
+    }
     console.log(index_y-1, + " " + x)
 }
 
 
 
-
+function get_computer_tile_index() {
+    let x = game_variables.computer_gameboard.div_board.indexOf(this)
+    let index_y = 0
+    while (x == -1) {
+        x = game_variables.computer_gameboard.div_board[index_y].indexOf(this)
+        index_y += 1
+    }
+    console.log(index_y-1, + " " + x)
+}
 
 
 function pick_ship_location(board, x, y) {
     let ship_list = game_variables.player_gameboard.ship_list 
     let correct_x_y = false
     while (correct_x_y == false){
-        if (y < 9 && x+ship_list[0] < 9 && y > -1 && x > -1){
+        if (y < 10 && x+ship_list[0] < 10 && y > -1 && x > -1){
             if (board[y][x].className != "ship_tile"){
                 if (ship_list != []){
                     board = place_ship(x, y+1, board, ship_list[0])
                     game_variables.player_gameboard.ship_list.shift();
                 }
-                    if (game_variables.player_gameboard.ship_list == []){
+                    if (game_variables.player_gameboard.ship_list.length == 0){
+                        game_variables.player_gameboard.ships_placed = true
                         correct_x_y = true
+                        game_manager()
                     }
                 
             else
@@ -175,6 +218,4 @@ function pick_ship_location(board, x, y) {
     return board
     }
 }
-
-
 
